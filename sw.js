639b6e60e7a9fcf19430e3666dec660a12e3e247
amir.goldental <amir.goldental@gmail.com>
2019-12-08 00:56:20 +0200

    PWA attempt 7

--- sw.js
+++ sw.js
@@ -1,41 +1,39 @@
-const staticCacheName = 'site-static-content';
-const assets = [
-    'index.html',
-    'css/style.css',
-    'images/favicon.ico',
-    'images/logo-512x512.png',
-    'images/logo-128x128.png',
-    'images/logo.png',
-    'images/BackGround.svg',
-    'fonts/Muli-Bold.ttf',
-    'fonts/Muli-Regular.ttf',
-    'fonts/Muli-SemiBold.ttf',
-];
-// install event
-self.addEventListener('install', evt => {
-    evt.waitUntil(
-        caches.open(staticCacheName).then((cache) => {
-            console.log('caching shell assets');
-            cache.addAll(assets);
+const version = "0.6.18";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                '',
+                'index.html',
+                'css/style.css',
+                'images/favicon.ico',
+                'images/logo-512x512.png',
+                'images/logo-128x128.png',
+                'images/logo.png',
+                'images/BackGround.svg',
+                'fonts/Muli-Bold.ttf',
+                'fonts/Muli-Regular.ttf',
+                'fonts/Muli-SemiBold.ttf',
+            ])
+                .then(() => self.skipWaiting());
         })
     );
 });
-// activate event
-self.addEventListener('activate', evt => {
-    evt.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => key !== staticCacheName)
-                .map(key => caches.delete(key))
-            );
-        })
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, { ignoreSearch: true }))
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
-// fetch event
-self.addEventListener('fetch', evt => {
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
-        })
-    );
-});
+
+
+
