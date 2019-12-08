2ba12639d088ee99affe0abac2c8afd18d5f5631
amir.goldental <amir.goldental@gmail.com>
2019-12-08 02:33:08 +0200

    Revert "PWA attempt 14"
    This reverts commit 35aeba01128dd0cc1c32ad2e2e61a4d9ec1cb535.

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-let deferredPrompt; // Allows to show the install prompt
-let setupButton;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    console.log("beforeinstallprompt fired");
-    if (setupButton == undefined) {
-        setupButton = document.getElementById("setup_button");
-    }
-    // Show the setup button
-    setupButton.style.display = "inline";
-    setupButton.disabled = false;
-});
-
-function installApp() {
-    // Show the prompt
-    deferredPrompt.prompt();
-    setupButton.disabled = true;
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('PWA setup accepted');
-                // hide our user interface that shows our A2HS button
-                setupButton.style.display = 'none';
-            } else {
-                console.log('PWA setup rejected');
-            }
-            deferredPrompt = null;
-        });
-}
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
         .then(reg => console.log('service worker registered'))
