ca1d4609b5f9e7754a85681c93ce0b5e094ebceb
amir.goldental <amir.goldental@gmail.com>
2019-12-09 23:44:26 +0200

    add install button

--- app.js
+++ app.js
@@ -7,3 +7,44 @@ if ('serviceWorker' in navigator) {
 function ResetForm() {
     window.location.reload(false);
 }
+
+// install button:
+window.addEventListener('load', function () {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        console.log('app runs in standalone mode');
+        document.getElementsByName("install_button_div")[0].style.display = "none";
+    } else {
+        console.log('app runs in browser');
+        document.getElementsByName("install_button_div")[0].style.display = "block";
+    }
+})
+
+
+let deferredPrompt = null;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+});
+
+async function install() {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        console.log(deferredPrompt)
+        deferredPrompt.userChoice.then(function (choiceResult) {
+
+            if (choiceResult.outcome === 'accepted') {
+                console.log('Your PWA has been installed');
+            } else {
+                console.log('User chose to not install your PWA');
+            }
+
+            deferredPrompt = null;
+
+        });
+
+
+    }
+}
