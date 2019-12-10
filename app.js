f83aee50a8457540990fb475b29c796599a48c0e
Amir <50057077+AmirGoldental@users.noreply.github.com>
2019-12-10 16:18:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ function InatallClick() {
         });
 }
 
-//window.addEventListener('load', function () {
-//    if (window.matchMedia('(display-mode: standalone)').matches) {
-//        console.log('app runs in standalone mode');
-//        document.getElementsByName("install_button_div")[0].style.display = "none";
-//    } else {
-//        console.log('app runs in browser');
-//        document.getElementsByName("install_button_div")[0].style.display = "block";
-//    }
-//})
+window.addEventListener('load', function () {
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        console.log('app runs in standalone mode');
+        document.getElementsByName("install_button_div")[0].style.display = "none";
+    } else {
+        console.log('app runs in browser');
+        document.getElementsByName("install_button_div")[0].style.display = "block";
+    }
+})
 
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
